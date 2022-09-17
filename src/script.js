a2d0f8e6947ffbc7896d7d97bd8093ed51087c7c
Gabriel-Vict0r <victorgabriel1730@gmail.com>
2022-09-17 12:45:04 -0300

    Generals alterations

--- src/script.js
+++ src/script.js
@@ -1,10 +1,29 @@
 //Function for insert the numbers e operators in the calculator
+
+function verifyLenghField(values) {
+  if (values.length > 5) { 
+    document.querySelector('.numbers').style.background = 'white';
+  }
+  else { 
+    document.querySelector('.numbers').style.background = 'orange';
+  }
+}
+
+
+var semValores = 'Sem valores...';
 function insert(atribute) {
+  document.querySelector('.numbers').style.webkitLineClamp = 2;
   var values = document.querySelector(".numbers").innerHTML;
+  if (document.querySelector('.numbers').innerHTML === semValores) {
+    values = '';
+  }
   document.querySelector(".numbers").innerHTML = values + atribute;
+  var lengthField = document.querySelector(`.numbers`).length;
+  verifyLenghField(lengthField);
 }
 //Realize the clean of the fields
 function clean() {
+  document.querySelector('.numbers').style.webkitLineClamp = 1;
   document.querySelector(".numbers").innerHTML = "";
   document.querySelector('.Calcs').innerHTML = "";
 }
@@ -27,12 +46,13 @@ function insertMinus() {
 //Make the calculation of the operation
 function calc() {
   var result = document.querySelector(".numbers").innerHTML;
+  document.querySelector('.numbers').style.webkitLineClamp = 1;
   if (result) {
     document.querySelector('.Calcs').innerHTML = result;
     var endResult = result.replace(/x/g, "*");
     document.querySelector('.numbers').innerHTML = eval(endResult);
   }
-  else { 
-    document.querySelector('.numbers').innerHTML = 'Sem valores...';
+  else {
+    document.querySelector('.numbers').innerHTML = semValores;
   }
 }
